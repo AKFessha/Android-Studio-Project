9f64bbab2c216182953a8687ae30874e66a0f6ed
Awet Fessha <awetkifle94@gmail.com>
2018-12-21 09:29:28 +0000

    First commit

---
+++ app/src/main/java/com/example/awet/gradeapp/SummaryGrade.kt
@@ -0,0 +1,12 @@
+package com.example.awet.gradeapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SummaryGrade : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_summary_grade)
+    }
+}
