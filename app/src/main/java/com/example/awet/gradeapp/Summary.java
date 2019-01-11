bd554beb74f839d56bad9cc1e82ca4e5724dd18b
Awet Fessha <awetkifle94@gmail.com>
2019-01-11 21:36:52 +0000

    added go back button

--- app/src/main/java/com/example/awet/gradeapp/Summary.java
+++ app/src/main/java/com/example/awet/gradeapp/Summary.java
@@ -2,12 +2,25 @@ package com.example.awet.gradeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
 
 public class Summary extends AppCompatActivity {
-
+  Button goBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary_grade);
+        goBack= (Button)findViewById(R.id.btnGoBack);
+      goBack.setOnClickListener(new View.OnClickListener(){
+
+          @Override
+          public void onClick(View view) {
+              Intent t = new Intent(getApplicationContext(), Main.class);
+              startActivity(t);
+          }
+      });
     }
 }
