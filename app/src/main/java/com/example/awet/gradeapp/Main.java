1bb00fef63c46e542af0729a621b2cc92c87946a
Awet Fessha <awetkifle94@gmail.com>
2019-01-18 21:18:25 +0000

    add  toast

--- app/src/main/java/com/example/awet/gradeapp/Main.java
+++ app/src/main/java/com/example/awet/gradeapp/Main.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Main extends AppCompatActivity {
     Button btnAdd, btnGoToSummary;
@@ -20,10 +21,13 @@ public class Main extends AppCompatActivity {
             public void onClick(View view) {
                 Intent  t = new Intent(getApplicationContext(), Summary.class);
                 startActivity(t);
+                Toast toast = Toast.makeText(getApplicationContext(), "This button has been clicked", Toast.LENGTH_LONG);
+                toast.setMargin(0,50);
+                toast.show();
             }
         });
 
-        btnAdd = (Button) findViewById(R.id.btnAddGrade);
+        btnAdd =  findViewById(R.id.btnAddGrade);
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
