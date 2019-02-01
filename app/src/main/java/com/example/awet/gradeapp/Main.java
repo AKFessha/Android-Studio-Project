0a86259fbc8ece3f55b0ac2746f583ba85ffeecc
Awet Fessha <awetkifle94@gmail.com>
2019-02-01 10:30:15 +0000

    check if the text input is null or repeated

--- app/src/main/java/com/example/awet/gradeapp/Main.java
+++ app/src/main/java/com/example/awet/gradeapp/Main.java
@@ -8,9 +8,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class Main extends AppCompatActivity {
     Button btnAdd, btnGoToSummary;
     EditText txtGrade;
+    String stGrade = null;
+    Double grade;
+    public static ArrayList<Double> listOfGrades= null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +23,26 @@ public class Main extends AppCompatActivity {
         btnGoToSummary = (Button) findViewById(R.id.btnShowSummary);
         btnAdd= findViewById(R.id.btnAddGrade);
         txtGrade= findViewById(R.id.txtGrade);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stGrade= txtGrade.getText().toString();
+                if(stGrade == null|| stGrade.equals("")){
+                   Toast tos = Toast.makeText(getApplicationContext(),"Please enter a grade ", Toast.LENGTH_LONG) ;
+                   tos.setMargin(0,50);
+                   tos.show();
+                }
+                else if(listOfGrades.contains(Double.parseDouble(stGrade))){
+                    Toast tos = Toast.makeText(getApplicationContext(),"Grade already exist", Toast.LENGTH_LONG) ;
+                    tos.setMargin(0,50);
+                    tos.show();
+                }
+                else{
+
+                }
+            }
+        });
 
         btnGoToSummary.setOnClickListener(new View.OnClickListener() {
             @Override
